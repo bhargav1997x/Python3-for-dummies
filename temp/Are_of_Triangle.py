d3d4bd18532405e918ac382a55249b21bc0c2dc7
dev-bhargav <bhargav1997x@gmail.com>
2018-03-09 18:59:39 +0530

    clean repo

---
+++ temp/Are_of_Triangle.py
@@ -0,0 +1,3 @@
+base = input("base of triangle")
+height = input("height of triangle")
+print (0.5*base*height)
---
+++ temp/triangle.py
@@ -0,0 +1,6 @@
+import math
+
+base = input("base of triangle")
+height = input("height of triangle")
+print(base)
+print(int(base)*int(height)*0.5)
